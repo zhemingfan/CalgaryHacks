efdef6b3c4af0dbf8c8e8bd7da821cdd5b5ead62
Jeremy Fan <zheming.fan@gmail.com>
2020-02-15 19:47:05 -0700

    Create practicescrap.py

---
+++ practicescrap.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+import urllib.request
+
+website = "http://contacts.ucalgary.ca/info/cpsc/contact-us/directory"
+page = urllib.request.urlopen(website) 
+
+print(page)
+soup = BeautifulSoup(page, features="html.parser")
+
+#print(soup.prettify())
+
+'''
+all = soup.find_all("Professor")
+for link in all:
+	print(link.get("href"))
+'''
+
+all_tables=soup.find_all('table')
+#print(all_tables)
+
+data = []
+
+'''
+for i in range(25): 
+	num = 'uofc-table-' + str(i+1)
+	table = soup.find('table', id=(num))
+
+	i = int(i) + 1
+	table_body = table.find('tbody')
+	rows = table_body.find_all('tr')
+
+	for row in rows:
+		cols = row.find_all('td')
+		cols = [ele.text.strip() for ele in cols]
+
+		for x in row.find_all('a', href = True):
+			data.append(x)
+			data.append([ele for ele in cols if cols]) # Get rid of empty values
+
+print(data)
+'''
+
