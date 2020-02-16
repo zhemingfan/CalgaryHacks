63605423490ac0ff0a07bc9cb75b52fb06558969
Jeremy Fan <zheming.fan@gmail.com>
2020-02-15 20:01:19 -0700

    Update practicescrap.py

--- practicescrap.py
+++ practicescrap.py
@@ -40,3 +40,27 @@ for i in range(25):
 print(data)
 '''
 
+for i in range(25): 
+	num = 'uofc-table-' + str(i+1)
+	table = soup.find('table', id=(num))
+
+	i = int(i) + 1
+	table_body = table.find('tbody')
+	rows = table_body.find_all('tr')
+
+	for row in rows:
+		cols = row.find_all('td')
+		cols = [ele.text.strip() for ele in cols]
+
+		for i in range(2):
+			for x in cols:
+				if x == '':
+					cols.remove(x)
+
+		for x in row.find_all('a', href = True):
+			cols.append(x)
+			data.append([ele for ele in cols if cols]) # Get rid of empty values
+
+print(data)
+
+
