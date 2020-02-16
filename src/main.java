f8ca29697d2ed4dcb0ddcd74f245976e4cef7823
Jenny Zhang <jennyzhangqianni@gmail.com>
2020-02-15 22:38:28 -0700

    Commented out JSoup

--- src/main.java
+++ src/main.java
@@ -1,39 +1,39 @@
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
+// import org.jsoup.Jsoup;
+// import org.jsoup.nodes.Document;
+// import org.jsoup.nodes.Element;
 
-class main {
-	public static void main(String[] args) {
-        System.out.println("Hello");
+// class main {
+// 	public static void main(String[] args) {
+//         System.out.println("Hello");
 
-        final String url = "https://sharestobeclosed.telegraph.co.uk/indices/financials/index/MCX";
-        try {
-            final Document document = Jsoup.connect(url).get();
+//         final String url = "https://sharestobeclosed.telegraph.co.uk/indices/financials/index/MCX";
+//         try {
+//             final Document document = Jsoup.connect(url).get();
             
-            for (Element row : document.select(
-                "table.tablesorter.full tr")) {
-                if (row.select("td:nth-of-type(1)").text().equals("")) {
-                    continue;
-                }
-                else {
-                   final String ticker = 
-                           row.select("td:nth-of-type(1)").text();
-                   final String name = 
-                           row.select("td:nth-of-type(2)").text();
-                   final String tempPrice = 
-                           row.select("td.right:nth-of-type(3)").text();
-                   final String tempPrice1 = 
-                           tempPrice.replace(",", "");
-//                    final double price = Double.parseDouble(tempPrice1);
+//             for (Element row : document.select(
+//                 "table.tablesorter.full tr")) {
+//                 if (row.select("td:nth-of-type(1)").text().equals("")) {
+//                     continue;
+//                 }
+//                 else {
+//                    final String ticker = 
+//                            row.select("td:nth-of-type(1)").text();
+//                    final String name = 
+//                            row.select("td:nth-of-type(2)").text();
+//                    final String tempPrice = 
+//                            row.select("td.right:nth-of-type(3)").text();
+//                    final String tempPrice1 = 
+//                            tempPrice.replace(",", "");
+// //                    final double price = Double.parseDouble(tempPrice1);
                    
-                    System.out.println(ticker + " " + name + " " + tempPrice1);
-                }
-            }
-        }
-        catch (Exception ex) {
-            ex.printStackTrace();
-        }
+//                     System.out.println(ticker + " " + name + " " + tempPrice1);
+//                 }
+//             }
+//         }
+//         catch (Exception ex) {
+//             ex.printStackTrace();
+//         }
 
-	}
-}
+// 	}
+// }
