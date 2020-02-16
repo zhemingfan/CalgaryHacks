f4effcd81bcdf2b918a4e15c59198fc5fbc0f045
Jeremy Fan <zheming.fan@gmail.com>
2020-02-15 21:31:36 -0700

    Edited files

--- practicescrap.py
+++ practicescrap.py
@@ -1,10 +1,10 @@
 from bs4 import BeautifulSoup
 import urllib.request
-
+import numpy as np
+import re
 website = "http://contacts.ucalgary.ca/info/cpsc/contact-us/directory"
 page = urllib.request.urlopen(website) 
-
-print(page)
+path = "/Users/owner/Documents/new.txt"
 soup = BeautifulSoup(page, features="html.parser")
 
 #print(soup.prettify())
@@ -40,6 +40,8 @@ for i in range(25):
 print(data)
 '''
 
+keyword_list = ['Professor']
+
 for i in range(25): 
 	num = 'uofc-table-' + str(i+1)
 	table = soup.find('table', id=(num))
@@ -52,15 +54,59 @@ for i in range(25):
 		cols = row.find_all('td')
 		cols = [ele.text.strip() for ele in cols]
 
+		cols = cols[0:2]
+
+		for x in row.find_all('a', href = True):
+			cols.append(x)
 		for i in range(2):
 			for x in cols:
 				if x == '':
 					cols.remove(x)
+				
 
-		for x in row.find_all('a', href = True):
-			cols.append(x)
-			data.append([ele for ele in cols if cols]) # Get rid of empty values
+		cols = cols[0:3]
+		#print(cols)
+
+		data.append([ele for ele in cols if cols]) # Get rid of empty values
+
+#print(data)
+
+
+
+
+
+'''
+with open(path, 'w') as file:
+	file.writelines('\t'.join(str(j) for j in i) + '\n' for i in data)
+'''
+
+
+############### Part 2, iterating through everyone's website ###############
+
+## filter for professors
+
+#if thing in some_list: some_list.remove(thing)
+print(data)
+print("\n")
+print("\n")
+
+print("\n")
+
+print("\n")
+
+keyword = "Professor"
+
+for x in data: 
+	if (keyword in x) == False:
+		data.remove(x)
+
 
 print(data)
+
+## loop through all professors, can grab research interests
+
+#page = urllib.request.urlopen(website) 
+
+
 
 
